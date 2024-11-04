eb8c9a618649003ee9c67ef0292947721cba451f
seonmooKang <156433780+seonmooKang@users.noreply.github.com>
2024-11-04 20:11:55 +0900

    security config 설정 완료

---
+++ src/main/java/com/example/walkfitoauth2/WalkfitOauth2Application.java
@@ -0,0 +1,13 @@
+package com.example.walkfitoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkfitOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalkfitOauth2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/walkfitoauth2/WalkfitOauth2ApplicationTests.java
@@ -0,0 +1,9 @@
+package com.example.walkfitoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalkfitOauth2ApplicationTests {
+
+}
